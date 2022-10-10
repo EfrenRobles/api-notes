811a26c9c970d77bb8896d9fdf523934636cf558
Efren Robles <efren_robles@hotmail.com>
2022-10-09 19:54:22 -0700

    * Adding index to migration 01_create_notes_table_09_10_2022.sql * Fixing issuge with properties due this was not set in the main.

--- src/main/java/api/ApiNotesApplication.java
+++ src/main/java/api/ApiNotesApplication.java
@@ -3,11 +3,15 @@ package api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import api.shared.infrastructure.config.EnvConfig;
+
 @SpringBootApplication
 public class ApiNotesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiNotesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        EnvConfig.setProperty(
+            SpringApplication.run(ApiNotesApplication.class, args).getEnvironment()
+        );
+    }
 
 }
